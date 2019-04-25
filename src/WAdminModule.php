d63eeede1c9c0a207f6d8d5009d703805454738c
wodrow-ys-pc <wodrow451611cv@gmail.com>
2019-04-25 17:43:44 +0800

    init

---
+++ src/WAdminModule.php
@@ -0,0 +1,11 @@
+<?php
+namespace wodrow\yii2wadmin;
+
+use yii\base\Module;
+
+
+
+class WAdminModule extends Module
+{
+    public $controllerNamespace = 'wodrow\yii2wadmin\controllers';
+}
---
+++ src/controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+namespace wodrow\yii2wadmin\controllers;
+
+
+use yii\web\Controller;
+
+class UserController extends Controller
+{
+    public function actionList()
+    {
+        echo 123456;
+    }
+}
